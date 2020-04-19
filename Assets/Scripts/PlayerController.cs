ab90aacf63a52f20ece6b25888bf5241177bc39e
Naz4h <skalblaka@gmail.com>
2020-04-19 18:31:07 +0200

    laser + missile + accell (19/04)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    private float timer = 0.0f;
+    public float timeAcceleration;
     public Camera cam;
     public Vector2 mousePos;
     public Rigidbody2D rb;
     Vector2 movement;
     public Vector2 MousePos;
+    public float accelCoolDown;
+    private bool enableAccel = true;
+
 
     // Update is called once per frame
     void Update()
@@ -28,6 +33,29 @@ public class PlayerController : MonoBehaviour
         Vector2 lookDir = MousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+        if (Input.GetButtonDown("Acceleration") && enableAccel == true)
+        {
+            acceleration();
+        }
+        timerAcceleration();
 
+    }
+    void acceleration()
+    {
+        moveSpeed += 10;
+        timer = 0.0f;
+        enableAccel = false;
+    }
+    void timerAcceleration()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeAcceleration)
+        {
+            moveSpeed = 5;
+        }
+        if(timer > accelCoolDown)
+        {
+            enableAccel = true;
+        }
     }
 }
