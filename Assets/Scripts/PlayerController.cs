61fcd8217e5c6529203e7a51fc765248f42c329b
Flentux <62889637+Flentux@users.noreply.github.com>
2020-04-19 19:22:11 +0200

    Dash19/04
    5 seconde stop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     Vector2 movement;
     public Vector2 MousePos;
+    public bool enableDash = true;
+    public float timer = 0.0f;
+    public float dashCoolDown = 5f;
 
     // Update is called once per frame
     void Update()
@@ -28,6 +31,25 @@ public class PlayerController : MonoBehaviour
         Vector2 lookDir = MousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+        if (Input.GetKeyDown(KeyCode.Space) && enableDash == true)
+        {
+            dash();
+        }
+        timerDash();
 
+    }
+    void dash()
+    {  
+        rb.position = MousePos;
+        timer = 0.0f;
+        enableDash = false;
+    }
+    void timerDash()
+    {
+        timer += Time.deltaTime;
+        if(timer > dashCoolDown)
+        {
+            enableDash = true;
+        }
     }
 }
