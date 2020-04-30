dc94b04136fcd0c82c816271ba49ed779452630b
Naz4h <skalblaka@gmail.com>
2020-04-30 18:40:03 +0200

    Shield working 30/04

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -8,32 +8,33 @@ public class Shield : MonoBehaviour
     public GameObject target;
     private Vector3 targetPosition;
     private bool enable = true;
-    private float timer = 0.0f;
+    private float timer= 0.0f;
     public float timeShield;
     public float shieldCoolDown;
-    
+    private GameObject shield;
+
+   
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetButtonDown("Shield") && enable == true) // Si le bouton correspondant à l'acceleration est apuyé et que le cool down de la compétecnce est fini
         {
             activate();
         }
+        timer1();
+        shield.transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
         
     }
     void activate()
     {
         targetPosition = target.transform.position;
-        GameObject shield = Instantiate(shieldPrefab, targetPosition,target.transform.rotation);
-        shield.transform.position = Vector3.Lerp(transform.position, targetPosition, 1500 * Time.deltaTime);
+        shield = Instantiate(shieldPrefab, targetPosition, target.transform.rotation);
+        enable = false;
+        timer = 0.0f;
     }
-    void timerAcceleration() //gère le temps de la compétence et le cool down
+    void timer1() //gère le temps de la compétence et le cool down
     {
         timer += Time.deltaTime; //le timer compte le temps passé 
-        if (timer > timeShield)
-        {
-            //Destroy(shield);
-        }
         if (timer > shieldCoolDown)
         {
             enable = true; //on permet de réutiliser la compétence
